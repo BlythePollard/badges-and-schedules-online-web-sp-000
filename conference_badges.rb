50034c85fb55eac6f3731473ccdb120f5e673358
Blythe Pollard <blytheepollard@gmail.com>
2019-09-07 21:22:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,6 @@ def printer(attendees)
   assign_rooms(attendees).each do |assignment|
     puts "#{assignment}"
 end
-printer(attendees)
 end
 
 #The method `printer` should output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen - this way you can output the badges and room assignments one at a time.
