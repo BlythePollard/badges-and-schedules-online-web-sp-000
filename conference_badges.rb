1c01cd7b1760e25f63d3bdce44481c8f8260d183
Blythe Pollard <blytheepollard@gmail.com>
2019-09-07 21:22:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badges|
-    puts badges}
+    puts badges
   end
   assign_rooms(attendees).each do |assignment|
     puts assignment
